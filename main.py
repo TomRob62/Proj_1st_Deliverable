9d96c55783e3f93a46f44713b75d072f391bb068
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-09-22 22:31:47 -0400

    feat. Identifies and prints tokens for a decaf program
    Added the main functions necessary for identifying and printing the tokens for the decaf language. Some tokens may be different as the assignment specifically said to match the example output. Affected tokens are T_TRUE and T_FALSE.

--- main.py
+++ main.py
@@ -1,23 +1,216 @@
 # Thomas Roberts
-# Scanner
+# Dr. S. Islam
+# CS 4308 (02) - Concepts of Programming Language
+# September 22, 2024
+
 import sys
 
+class Scanner:
+    """
+    Scanner for the decaf programming language
+
+    Functions
+    ---------
+    __init__(file)
+    lookup(find_char)
+    """
+    # global variables
+    char_classes = ('LETTER', 'DIGIT', 'UNKNOWN', 'EOF')
+    next_char = ''
+    next_char_class = 0
+    lexeme = []
+    token_class = ""
+    col_num = 0
+    line_num = 0
+    current_line = ""
+    file = []
+    operators = ('+','-','*','/','(', ')', '{', '}','[',']',';','=',',','!', )
+    token_name = ('T_AND', 'T_ASSIGN' ,'T_BOOLTYPE', 'T_BREAK', 'T_CHARCONSTANT',
+                  'T_COMMA', 'T_COMMENT', 'T_CONTINUE', 'T_DIV', 'T_DOT','T_ELSE', 
+                  'T_EQ', 'T_EXTERN', 'T_BoolConstant (value= false)', 'T_FOR', 'T_FUNC', 'T_GEQ','T_GT', 
+                  'T_ID', 'T_IF','T_INTCONSTANT', 'T_Int', 'T_LCB', 'T_LEFTSHIFT', 
+                  'T_LEQ', 'T_LPAREN', 'T_LSB', 'T_LT', 'T_MINUS', 'T_MOD', 'T_MULT', 
+                  'T_NEQ', 'T_NOT', 'T_NULL', 'T_OR', 'T_PACKAGE', 'T_PLUS', 'T_RCB',
+                  'T_RETURN', 'T_RIGHTSHIFT', 'T_RPAREN', 'T_RSB', 'T_SEMICOLON', 
+                  'T_STRINGCONSTANT', 'T_STRINGTYPE', 'T_BoolConstant (value= true)', 'T_VAR', 'T_VOID', 
+                  'T_WHILE', 'T_WHITESPACE')
+    token_value = ('&&', '=', 'bool', 'break', 'char_lit', ',', 'comment', 'continue', 
+                   '/', '.', 'else', '==', 'extern', 'false', 'for', 'func', '>=', '>', 
+                    'identifier', 'if', 'int_lit', 'int', '{', '<<', '<=', '(', '[', '<', 
+                    '-', '%', '*', '!=', '!', 'null', '||', 'package', '+', '}', 'return',
+                    '>>', ')', ']', ';', 'string_lit', 'string', 'true', 'var', 'void',
+                    'while', 'whitespace')
+    
+
+    def __init__(self, file) -> None:
+        """
+        Initializer function for Scanner obj
+
+        Parameters
+        ----------
+        file - The file (in decaf language) being scanned
+        """
+        self.file = file.readlines()
+        self.current_line = self.file[0]
+    # end __init__()
+
+    def add_char(self) -> None:
+        """
+        Helper function that appends next_char to lexeme list
+        """
+        self.lexeme = self.lexeme + self.next_char
+    # end of add_char()
+
+    def get_char(self) -> None:
+        """
+        Helper function that checks if the next char is a digit, alpha, unknown, or EOF.
+        Increments line_num, col_num as needed.
+        """ 
+        current_char = ''
+        if self.col_num >= len(self.current_line): # checks for end of string
+            if self.line_num < len(self.file)-1: # checks for EOF
+                self.line_num = self.line_num+1 # incremenets line_num
+                self.col_num = 0
+                self.current_line = self.file[self.line_num]
+            else: # goes to next string in file
+                self.next_char = ''
+                self.next_char_class = 3 # EOF
+                return
+        # end if
+        current_char = self.current_line[self.col_num] 
+        self.next_char = current_char # updates next char
+        self.col_num = self.col_num+1 # increments col_num
+        if current_char.isalpha(): # LETTER
+            self.next_char_class = 0
+            return 
+        elif current_char.isdigit(): # DIGIT
+            self.next_char_class = 1
+            return 
+        else:
+            self.next_char_class = 2 # UNKNOWN
+            return 
+    # end of get_char()
+
+    def get_non_blank(self) -> None:
+        """
+        Helper method that calls get_char until a nonblank is found
+        """
+        while(self.next_char.isspace()):
+            self.get_char()
+    # end of get_non_blank()
+
+    def lookup(self, find_char) -> int:
+        """
+        lookup function to find the token_id for a given character
+
+        Paramaters
+        ----------
+        find_char - character that needs to be identified
+
+        Returns
+        -------
+        int - index of token. Reference Scanner.token_name and Scanner.token_value
+        """
+        next_token = 'NOTFOUND'
+        if self.token_value.__contains__(find_char):
+            token_index = self.token_value.index(find_char)
+            self.token_class = self.token_name[token_index]
+            next_token = self.token_class
+        # end if
+        return next_token 
+    # end of lookup()
+
+    def peak(self, char1) -> bool:
+        """
+        Helper method that determines if a possible combination of 
+        unknown characters could form a known token
+        """
+        if self.col_num < len(self.current_line):
+            test_char = char1 + self.current_line[self.col_num-1]
+        else:
+            test_char = ""
+        result = self.lookup(test_char)
+        return not result == 'NOTFOUND'
+    # end peak()
+
+    def lex(self) -> None:
+        self.lexeme = ""
+        self.get_non_blank()
+
+        if self.next_char_class == 0: # CASE 1 LETTER
+            self.add_char()
+            self.get_char()
+            while self.next_char_class == 0 or self.next_char_class == 1:
+                self.add_char()
+                self.get_char()
+            current_token = self.lookup(self.lexeme) # checks for known token such as 'void' = 'T_VOID'
+            if current_token == 'NOTFOUND':
+                self.token_class = 'T_IDENTIFIER'
+        elif self.next_char_class == 1: # CASE 2 DIGIT
+            self.add_char()
+            self.get_char()
+            while self.next_char_class == 1:
+                self.add_char()
+                self.get_char()
+            self.token_class = 'T_INTCONSTANT (value= %s)' % self.lexeme
+        elif self.next_char_class == 2: # CASE 3 UNKNOWN
+            if self.next_char == '"' or self.next_char == '\'': # Checks for string constants
+                conditional = True
+                while conditional:
+                    self.add_char()
+                    self.get_char()
+                    conditional = not (self.next_char == '"' or self.next_char == '\'')
+                self.add_char()
+                self.get_char()
+                self.token_class = 'T_STRINGCONSTANT (value= %s)' % self.lexeme
+            else: # not a string constant
+                self.add_char()
+                self.get_char()
+                current_token = self.lookup(self.lexeme)
+                while self.next_char_class == 2 and current_token == 'NOTFOUND':
+                    self.add_char()
+                    self.get_char()
+                    current_token = self.lookup(self.lexeme)
+                if self.peak(self.lexeme): # peaks for a known combinations. For example '<=' is a known token. so is '<' and '=' by themselves
+                    self.add_char()
+                    self.get_char()
+                    current_token = self.lookup(self.lexeme)
+                if self.operators.__contains__(self.lexeme):
+                    self.token_class = "\'%s\'" % self.lexeme
+        else: # CASE 4 EOF
+            self.lexeme = "EOF0"
+        
+        print("%s     line %s Cols  %s - %s  is  %s" % (self.lexeme, 
+                                                  self.line_num+1, 
+                                                  (self.col_num-len(self.lexeme)), 
+                                                  self.col_num-1, 
+                                                  self.token_class))
+# end of class Scanner
+
 def main() -> None:
-    # description
-    file_name = ""
+    """
+    Program driver function
+    """
+    # try to open file
     try:
-        file_name = sys.argv[1]
-        my_file = open(file_name, 'r')
+        file_name = sys.argv[1] # check for file name
+        my_file = open(file_name, 'r') # open file
+        scan_obj = Scanner(my_file) # pass IO stream to scanner
     except IndexError:
         print("file argument not given")
         sys.exit()
     except FileNotFoundError:
         print("file not found.")
         sys.exit()
+    # end of try/except
 
-def scanner() -> None:
-    # description
-    print("done")
-    
-if __name__ == 'main':
+    # body of main function
+    scan_obj.get_char()
+    conditional = True
+    while(conditional):
+        scan_obj.lex()
+        conditional = (scan_obj.next_char_class != 3) 
+# end of main()
+
+if __name__ == '__main__':
     main()
