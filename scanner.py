6f0cc5997eb95703f2939aa674e3bd38627260c1
TomRob62 <139529054+TomRob62@users.noreply.github.com>
2024-10-29 19:20:28 -0400

    fix token label for int type

--- scanner.py
+++ scanner.py
@@ -43,7 +43,7 @@ class Scanner:
     token_name = ('T_AND', 'T_ASSIGN' ,'T_BOOLTYPE', 'T_BREAK', 'T_CHARCONSTANT',
                   'T_COMMA', 'T_COMMENT', 'T_CONTINUE', 'T_DIV', 'T_DOT','T_ELSE', 
                   'T_EQ', 'T_EXTERN', 'T_BoolConstant (value= false)', 'T_FOR', 'T_FUNC', 'T_GEQ','T_GT', 
-                  'T_ID', 'T_IF','T_INTCONSTANT', 'T_Int', 'T_LCB', 'T_LEFTSHIFT', 
+                  'T_ID', 'T_IF','T_INTCONSTANT', 'T_INTTYPE', 'T_LCB', 'T_LEFTSHIFT', 
                   'T_LEQ', 'T_LPAREN', 'T_LSB', 'T_LT', 'T_MINUS', 'T_MOD', 'T_MULT', 
                   'T_NEQ', 'T_NOT', 'T_NULL', 'T_OR', 'T_PACKAGE', 'T_PLUS', 'T_RCB',
                   'T_RETURN', 'T_RIGHTSHIFT', 'T_RPAREN', 'T_RSB', 'T_SEMICOLON', 
